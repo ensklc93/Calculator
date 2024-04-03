6dfaee3547ffadb6363997d8cc131135fad76c97
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-04-03 16:51:37 +0200

    Added the first version of the Add operator Creating some array to store the input values Pushing the values to first array to clear the second array for new input

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 function add (n) {
-    return n.reduce((a,b) => a + b, 0)
+    return n.reduce((a,b) => a + b, 0);
 }
 
-function substract (n) {
+function subtract (n) {
     return n.reduce((a,b) => a - b, 0)
 }
 
@@ -14,17 +14,59 @@ function divide (n) {
     return n.reduce((a,b) => a / b, 1)
 }
 
-let firstNumber;
-let secondNumber;
+let firstNumber = []
+let secondNumber = [];
 let operator;
 
 
 function operate (firstNumber,secondNumber,operator) {
-    let x = []
-    x.push(firstNumber)
-    x.push(secondNumber)
-    return operator(x)
+
+
+
 }
 
+const container = document.querySelector(".container")
+const digits = document.querySelectorAll(".digits")
+const input = document.querySelector("#calc")
+const addOperator = document.querySelector("#addOperator")
+const subtractOperator = document.querySelector('#subtract')
+const resultScreen = document.querySelector('#result')
+const equality = document.querySelector('#equal')
+
+for (let i = 0; i < digits.length; i++) {
+    digits[i].addEventListener("click", () => {
+        input.value += digits[i].value
+    });
+}
+
+addOperator.addEventListener('click', () => {
+    if(firstNumber.length < 1) {
+    firstNumber.push(parseInt(input.value))
+    } else if (firstNumber.length < 2) {
+        secondNumber.push(parseInt(input.value))
+        if(secondNumber.length == 1) {
+            let operatorArray = firstNumber.concat(secondNumber)
+            let resultAdd = add(operatorArray);
+            resultScreen.innerHTML = `${firstNumber} + ${secondNumber} = ${resultAdd}`;
+            firstNumber.splice(0,1, resultAdd)
+            secondNumber = []
+        }
+    }
+    console.log(firstNumber)
+    console.log(secondNumber)
+    
+})
 
 
+
+
+
+
+
+// equality.addEventListener('click', () => {
+
+//     console.log(firstNumber)
+//     secondNumber.push(parseInt(input.value))
+//     console.log(secondNumber);
+
+// })
