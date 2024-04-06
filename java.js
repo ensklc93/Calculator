c6516d95e346ea23b55704084665b40873bd1438
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-04-06 02:13:01 +0200

    Added the function for the main buttons & optimizing Adding the backspace function Making a main function for the operators and referencing to the operator functions Removing code duplications Adding a function to only allow numbers and a single comma to input Adding a function to set the focus on the input field

--- java.js
+++ java.js
@@ -9,7 +9,12 @@ function multiply (firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
 function divide (firstNumber, secondNumber) {
+    if(firstNumber == 0 || secondNumber == 0) {
+        resultScreen.innerHTML = "That is a dangerous path you're walking fella!"
+        return operationArray.splice(0)
+    } else{
     return  firstNumber / secondNumber;
+    }
 }
 
 
@@ -37,126 +42,85 @@ for (let i = 0; i < digits.length; i++) {
     });
 }
 
+input.addEventListener("blur", function(event){
+    input.focus();
+}); 
 
 
-addOperator.addEventListener('click', () => {
-    operatorValue = addOperator.value
-     if (operationArray.length < 1) {
-        operator = add;
+function clickOperation (operatorPlaceholder) {
+    if (operationArray.length < 1) {
+        if(isNaN(input.value) == true) {
+            return
+        } else {
+        operator = operatorPlaceholder;
         operationArray[0] = parseInt(input.value)
-        resultScreen.innerHTML = `${operationArray[0]} +`
+        resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
         input.value = "";
         input.select();
+        }
     } else if (operationArray.length < 2) {
-        if(input.value == false) {
-            resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-            return operationArray[1] = "";
+        if(isNaN(input.value) == true) {
+             resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
+             return operator = operatorPlaceholder;
         } else {
             operationArray[1] = parseInt(input.value)
             console.log(operationArray)
             let sum = operationArray.reduce(operator)
-            operator = add;
-            resultScreen.innerHTML = `${sum} +`
+            resultScreen.innerHTML = `${sum} ${operatorValue}`
+            operator = operatorPlaceholder;
             operationArray.splice(0,2, sum)
             console.log(operationArray)
             input.value = "";
             input.select();
         }
     }
+
+}
+
+addOperator.addEventListener('click', () => {
+    operatorValue = addOperator.value
+    clickOperation(add)
 })
 
 subtractOperator.addEventListener('click', () => {
     operatorValue = subtractOperator.value
-    if (operationArray.length < 1){
-        operator = subtract;
-        operationArray[0] = parseInt(input.value)
-        resultScreen.innerHTML = `${operationArray[0]} -`
-        input.value = "";
-        input.select();
-    } else if (operationArray.length < 2) {
-        if(input.value == false) {
-            resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-            operationArray[1] = "";
-        } else {
-            operationArray[1] = parseInt(input.value)
-            console.log(operationArray)
-            let sum = operationArray.reduce(operator)
-            operator = subtract;
-            resultScreen.innerHTML = `${sum} -`
-            console.log(operationArray)
-            operationArray.splice(0,2, sum)
-            input.value = "";
-            input.select();
-        }
-    }
+    clickOperation(subtract)
 })
 
 multiplyOperator.addEventListener('click', () => {
     operatorValue = multiplyOperator.value
-     if(operationArray.length < 1){
-        operator = multiply;
-        operationArray[0] = parseInt(input.value)
-        resultScreen.innerHTML = `${operationArray[0]} *`
-        input.value = "";
-        input.select();
-    } else if (operationArray.length < 2) {
-        if(input.value == false) {
-            resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-            return operationArray[1] = "";
-        } else {
-            operationArray[1] = parseInt(input.value)
-            console.log(operationArray)
-            let sum = operationArray.reduce(operator)
-            operator = multiply;
-            resultScreen.innerHTML = `${sum} *`
-            operationArray.splice(0,2, sum)
-            console.log(operationArray)
-            input.value = "";
-            input.select();
-        }
-    }
+    clickOperation(multiply)
 })
 
 divideOperator.addEventListener('click', () => {
     operatorValue = divideOperator.value
-     if(operationArray.length < 1){
-        operator = divide;
-        operationArray[0] = parseInt(input.value)
-        resultScreen.innerHTML = `${operationArray[0]} /`
-        input.value = "";
-        input.select();
-    } else if (operationArray.length < 2) {
-        if(input.value == false) {
-            resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-            operationArray[1] = "";
-        } else {
-            operationArray[1] = parseInt(input.value)
-            console.log(operationArray)
-            let sum = operationArray.reduce(operator)
-            operator = divide;
-            resultScreen.innerHTML = `${sum} /`
-            operationArray.splice(0,2, sum)
-            console.log(operationArray)
-            input.value = "";
-            input.select();
-        }
-    }
+    clickOperation(divide)
+    
 })
 
-clear.addEventListener('click', () => operationArray.splice(0))
+clear.addEventListener('click', () => {
+    operationArray.splice(0)
+    resultScreen.innerHTML =""
+    input.value = "";
+})
 
 function operate () {
-    if(operationArray.length < 1 || operationArray == isNaN) {
+    if(operationArray.length < 1) {
         input.value = "";
         input.select();
     } else {
         operationArray[1] = parseInt(input.value)
+        console.log(operationArray)
+        if(isNaN(operationArray[1]) == true) {
+            return operationArray.splice(1,1)
+        } else {
         let result = operator(operationArray[0], operationArray[1])
         resultScreen.innerHTML = `${operationArray[0]} ${operatorValue} ${operationArray[1]} = ${result}` 
         let sum = operationArray.reduce(operator)
         operationArray.splice(0,2, sum)
         input.value = "";
         input.select();
+        }
     }
 
 console.log(operationArray)
@@ -164,3 +128,14 @@ console.log(operationArray)
 
 equality.addEventListener('click', () => operate())
 
+backspace.addEventListener('click', () => {input.value = input.value.substring(0, input.value.length-1)})
+
+
+input.oninput = checkKey;
+function checkKey() {
+    let clean = this.value.replace(/[^0-9,]/g, "")
+                           .replace(/(,.*?),(.*,)?/, "$1");
+    // don't move cursor to end if no change
+    if (clean !== this.value) this.value = clean;
+    
+}
