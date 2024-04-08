daed2c9ea0529bf9b6878d7f9c4e89f8a1c7b95c
ensklc93 <143909013+ensklc93@users.noreply.github.com>
2024-04-08 14:53:19 +0200

    Added keyboard support Adding event.keycode Changing parseInt to parseFloat to enable decimal calculations Restricting the usage of dot to only once every calculations Added syntax for dividing with 0 to prevent errors

--- java.js
+++ java.js
@@ -11,7 +11,7 @@ function multiply (firstNumber, secondNumber) {
 function divide (firstNumber, secondNumber) {
     if(firstNumber == 0 || secondNumber == 0) {
         resultScreen.innerHTML = "That is a dangerous path you're walking fella!"
-        return operationArray.splice(0)
+        firstNumber + secondNumber;
     } else{
     return  firstNumber / secondNumber;
     }
@@ -33,14 +33,28 @@ const resultScreen = document.querySelector('#result')
 const equality = document.querySelector('#equal')
 const clear = document.querySelector('#clear')
 const backspace = document.querySelector('#backspace')
+const dot = document.querySelector('#dot')
 
 
 // After selecting the buttons adding the button value to input value
 for (let i = 0; i < digits.length; i++) {
     digits[i].addEventListener("click", () => {
-        input.value += digits[i].value
+        if(input.value.length < 10) {
+            input.value += digits[i].value
+        } else {
+            return
+        }
     });
 }
+// Allowing the dot button once in every calculation 
+dot.addEventListener("click", () => {
+    if(input.value.length > 0 && input.value.indexOf('.') > -1 ) {
+        return
+    } else {
+        input.value += dot.value
+    }
+})
+
 
 input.addEventListener("blur", function(event){
     input.focus();
@@ -49,29 +63,37 @@ input.addEventListener("blur", function(event){
 
 function clickOperation (operatorPlaceholder) {
     if (operationArray.length < 1) {
-        if(isNaN(input.value) == true) {
-            return
-        } else {
         operator = operatorPlaceholder;
-        operationArray[0] = parseInt(input.value)
-        resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-        input.value = "";
-        input.select();
-        }
-    } else if (operationArray.length < 2) {
-        if(isNaN(input.value) == true) {
-             resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
-             return operator = operatorPlaceholder;
+        operationArray[0] = parseFloat(input.value)
+        if (isNaN(operationArray[0]) == true){
+            return operationArray.splice(0)
         } else {
-            operationArray[1] = parseInt(input.value)
-            console.log(operationArray)
-            let sum = operationArray.reduce(operator)
-            resultScreen.innerHTML = `${sum} ${operatorValue}`
-            operator = operatorPlaceholder;
-            operationArray.splice(0,2, sum)
-            console.log(operationArray)
+            resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
             input.value = "";
             input.select();
+        }
+    } else if (operationArray.length < 2) {
+        operationArray[1] = parseFloat(input.value)
+            if(isNaN(operationArray[1]) == true) {
+                operationArray.splice(1,1)
+                resultScreen.innerHTML = `${operationArray[0]} ${operatorValue}`
+                return operator = operatorPlaceholder;
+           } else {
+            console.log(operationArray)
+            let sum = operationArray.reduce(operator)
+            if(isNaN(sum) == true || sum == undefined) {
+                operator = operatorPlaceholder;
+                operationArray.splice(0)
+                input.value = "";
+                input.select();
+            } else {
+                resultScreen.innerHTML = `${sum} ${operatorValue}`
+                operator = operatorPlaceholder;
+                operationArray.splice(0,2, sum)
+                console.log(operationArray)
+                input.value = "";
+                input.select();
+            }
         }
     }
 
@@ -105,11 +127,13 @@ clear.addEventListener('click', () => {
 })
 
 function operate () {
-    if(operationArray.length < 1) {
+    if(operationArray.length < 1 || isNaN(operationArray[0]) == true) {
         input.value = "";
         input.select();
+        operationArray.splice(0)
+        resultScreen.innerHTML = ""
     } else {
-        operationArray[1] = parseInt(input.value)
+        operationArray[1] = parseFloat(input.value)
         console.log(operationArray)
         if(isNaN(operationArray[1]) == true) {
             return operationArray.splice(1,1)
@@ -122,9 +146,29 @@ function operate () {
         input.select();
         }
     }
-
-console.log(operationArray)
 }
+
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        equality.click();
+    } else if (event.keyCode === 187 && event.shiftKey == true) {
+        multiplyOperator.click();
+    } else if (event.keyCode === 187) {
+        addOperator.click();
+    }
+    else if (event.keyCode === 189) {
+        subtractOperator.click();
+    }
+    else if (event.keyCode === 55) {
+        divideOperator.click();
+    }
+});
+
+
+
+
+
 
 equality.addEventListener('click', () => operate())
 
@@ -133,8 +177,8 @@ backspace.addEventListener('click', () => {input.value = input.value.substring(0
 
 input.oninput = checkKey;
 function checkKey() {
-    let clean = this.value.replace(/[^0-9,]/g, "")
-                           .replace(/(,.*?),(.*,)?/, "$1");
+    let clean = this.value.replace(/[^.\d]/g, '')
+                          .replace(/^(\d*\.?)|(\d*)\.?/g, "$1$2");
     // don't move cursor to end if no change
     if (clean !== this.value) this.value = clean;
     
